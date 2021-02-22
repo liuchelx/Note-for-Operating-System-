23a4ccf68da98719b6590e86100c717d1f0e9885
liuchelx <liuchelx@gmail.com>
2021-02-22 15:36:29 -0800

    all not for week4

--- Operating System and you/Note for week4.py
+++ Operating System and you/Note for week4.py
@@ -24,4 +24,39 @@ linux:
     mklabel gpt #set the disk label with gpt partition
     mkpart primary ext4 1Mib 5Gib # the command used to slice the disk with factor
     quit # quit
-    
+
+    mount #  这个命令是干啥的？
+    sudo mount /dev/sdb1/my_usb/  #create a accessable directory so we use new file system
+    umount #uninstall the file system
+    /etc/fstab #the direc we used to mount a disk permanently
+    sudo blkid #to get the block id , which is the uuid for the disk
+
+    swap space
+    enter the parted terminal, then use mkpart primary linux-swap 5GiB 100% #100% means use all the avaiable space
+    quit the parted, then 
+    sudo mkswap /dev/sdb2
+    sudo swap on /dev/sdb2
+    #if we want auto boot a disk every time, then add a swap item in the /etc fstab
+
+windows:
+    MFT(master file table) #each file has at least one record in MFT, usually it's one on one. but if a file may has more.
+    MFT:
+        file name  
+        timestamp
+        Permission
+        compression
+        location
+        etc.....
+    file record number: the index of MFT
+    symbolic link # similar to short cut, but what is the differece????
+    mklink #we can use this command to make a symbolic link , follwed by -H, will make a hrad link, 
+    #hard link means even change the name, the short cut still point to the orgin file
+
+linxu:
+    inode#similar to the MFT of windows, contain all the 
+    softlink # point to a file
+    hardlink # point to a inode in inode table
+    ln <filename> <filename_hardlink >
+    ls -l <filename># the last digit, which is a number, indicate the amount of hard link of the file
+    du -h # to check the usage of the disk, -h make it readable
+    df-h # disk free, how much free disk on your disk
