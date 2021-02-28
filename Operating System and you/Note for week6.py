a30910caa67300a7038e3eddcd3a10c929e6fb86
liuchelx <liuchelx@gmail.com>
2021-02-27 21:59:20 -0800

    note for week 5 and week 6

---
+++ Operating System and you/Note for week6.py
@@ -0,0 +1,16 @@
+SSH: secure shell
+RDP:remote Desktop Protocol
+
+SCP(Secure copy): a command you can use in Linux to copy files betweem computers on a network
+scp <file direction> <username>@<ipaddress>: #transfer file through command
+
+/var/log #the location where linux store the logs
+/var/log/auth.log #authorization and security-related events 
+/var/log/kern.log #kernel message
+/var/log/dmesg #system startup message
+/var/log/syslog #logs everything
+
+logrotate #utility rotate logs in linux
+centralized logging #parse different systems' log inn one central location
+
+unix(or epoch time)
